6484b82b01f2be2b325a1372e30e7b8c67018039
s-weigand <s.weigand.phy@gmail.com>
2022-03-08 12:49:36 +0100

    ✨ Add support for qt5-applications and qt6-applications

--- qt_dev_helper/qt_tools.py
+++ qt_dev_helper/qt_tools.py
@@ -1,10 +1,12 @@
 """Qt implementation cross compatibility module."""
 from __future__ import annotations
 
+import os
 import shutil
 import subprocess
 from collections.abc import Sequence
 from functools import lru_cache
+from importlib.resources import path as package_path
 from pathlib import Path
 
 
@@ -39,6 +41,27 @@ class QtToolExecutionError(Exception):
         )
 
 
+@lru_cache(maxsize=1)
+def extend_qt_tool_path() -> str:
+    """Prepend path variable with package dirs to qt-tools if present.
+
+    Returns
+    -------
+    str
+        _description_
+    """
+    additional_paths: list[str] = []
+    # package_name: rel_path_to_include
+    tool_packages = {"PySide6": "", "qt5_applications": "Qt/bin", "qt6_applications": "Qt/bin"}
+    for package, rel_path in tool_packages.items():
+        try:
+            with package_path(package, "__init__.py") as p:
+                additional_paths.append(str(p.parent / rel_path))
+        except ModuleNotFoundError:
+            pass
+    return os.pathsep.join((*additional_paths, os.environ.get("path", "")))
+
+
 @lru_cache()
 def find_qt_tool(tool_name: str) -> str:
     """Find path to Qt tool executable like ``rcc``, ``uic`` or ``designer``.
@@ -58,11 +81,10 @@ def find_qt_tool(tool_name: str) -> str:
     QtToolNotFoundError
         If the tool could not be found in the path.
     """
-    known_prefixes = ["pyside6-", ""]
-    for known_prefix in known_prefixes:
-        command_path = shutil.which(f"{known_prefix}{tool_name}")
-        if command_path is not None:
-            return Path(command_path).as_posix()
+    extended_path = extend_qt_tool_path()
+    command_path = shutil.which(tool_name, path=extended_path)
+    if command_path is not None:
+        return Path(command_path).as_posix()
     raise QtToolNotFoundError(tool_name)
 
 
--- tests/test_qt_tools.py
+++ tests/test_qt_tools.py
@@ -1,6 +1,8 @@
 """Tests for ``qt_dev_helper.qt_tools``."""
 from __future__ import annotations
 
+import os
+
 import pytest
 from _pytest.capture import CaptureFixture
 from _pytest.monkeypatch import MonkeyPatch
@@ -8,18 +10,40 @@ from _pytest.monkeypatch import MonkeyPatch
 from qt_dev_helper.qt_tools import QtToolExecutionError
 from qt_dev_helper.qt_tools import QtToolNotFoundError
 from qt_dev_helper.qt_tools import call_qt_tool
+from qt_dev_helper.qt_tools import extend_qt_tool_path
 from qt_dev_helper.qt_tools import find_qt_tool
 
 
+@pytest.mark.parametrize("package_name", ("pyside6", "qt5_applications", "qt6_applications"))
+def test_extend_qt_tool_path(package_name: str):
+    """All supported tools are on the extended path"""
+    assert package_name in extend_qt_tool_path().lower()
+
+
+def test_extend_qt_tool_path_missing_module(monkeypatch: MonkeyPatch):
+    """Return path if none of the modules is installed."""
+    with monkeypatch.context() as m:
+        from qt_dev_helper import qt_tools
+
+        def mock_package_path(*args):
+            raise ModuleNotFoundError()
+
+        m.setattr(qt_tools, "package_path", mock_package_path)
+
+        assert extend_qt_tool_path.__wrapped__() == os.environ.get("path", "")
+
+
 def test_find_qt_tool():
-    """When PySide6 is installed use pyside6-rcc"""
-    assert "pyside6-rcc" in find_qt_tool("rcc")
+    """When PySide6 is installed use rcc from the PySide6 package."""
+    assert "site-packages/pyside6/rcc" in find_qt_tool("rcc").lower()
 
 
 def test_find_qt_tool_not_found(monkeypatch: MonkeyPatch):
     """Raise error if qt tool can't be found."""
     with monkeypatch.context() as m:
-        m.setenv("path", "")
+        from qt_dev_helper import qt_tools
+
+        m.setattr(qt_tools, "extend_qt_tool_path", lambda: "")
         with pytest.raises(QtToolNotFoundError) as exc_info:
             find_qt_tool.__wrapped__("rcc")
 
