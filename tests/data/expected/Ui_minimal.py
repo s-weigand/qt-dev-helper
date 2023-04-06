56478ddb2c869211f0926e82d5f94d93674316e7
s-weigand <s.weigand.phy@gmail.com>
2023-04-06 15:11:28 +0200

    🩹 Adjust for removed removed additional space in PySide6-Essentials

--- tests/data/expected/Ui_minimal.py
+++ tests/data/expected/Ui_minimal.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
     QSizePolicy, QStatusBar, QWidget)
-from  . import test_resource_rc
+from . import test_resource_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
